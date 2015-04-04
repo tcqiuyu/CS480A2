688a5ce106173c4198fa6125a749cc9b5b483d9c
Yu Qiu <tcqiuyu2@gmail.com>
2015-04-03 18:06:55 -0600

    map output != reduce input???

--- src/cs480a2/yqiu/recSystem/mapreduce/CustomReducer.java
+++ src/cs480a2/yqiu/recSystem/mapreduce/CustomReducer.java
@@ -1,10 +1,12 @@
 package cs480a2.yqiu.recSystem.mapreduce;
 
 import cs480a2.yqiu.recSystem.mapreduce.structure.TextArrayWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by Qiu on 3/18/15.
@@ -31,30 +33,30 @@ public class CustomReducer extends Reducer<Text, TextArrayWritable, Text, Text> 
     public void reduce(Text key, Iterable<TextArrayWritable> values, Context context) throws IOException, InterruptedException {
         //this collection contains information for a certain word in each book that contains this word
         //information: [ book title that contains this word, word occurance, maximum word occurance for this book ]
-//        HashMap<TextArrayWritable, Integer> map = new HashMap();
+        HashMap<TextArrayWritable, Integer> map = new HashMap<>();
 //
         for (TextArrayWritable val : values) {
-//            map.put(val, 0);
-            throw new IOException("Key: " + key + " --- Val: " + val.toString());
+            map.put(val, 0);
+//            throw new IOException("Key: " + key + " --- Val: " + val.toString());
         }
-//
-//        //calculate the number of books this word occurs
-//        double bookOccurCount = map.size();
-//
-////        Iterator<TextArrayWritable> iterator = values.iterator();
-//        for (TextArrayWritable val : map.keySet()) {
-//            Text title = (Text) val.get()[0];
-//            Double wordCount = Double.parseDouble(val.get()[1].toString());
-//            Double maxWordCount = Double.parseDouble(val.get()[2].toString());
-//            //calculate TF-IDF value
-//            Double tf = wordCount / maxWordCount;
-//            Double idf = Math.log(totalBookCount / bookOccurCount) / Math.log(2);
-//            DoubleWritable tfidf = new DoubleWritable(tf / idf);
-//
-//            String output = title.toString() + ":" + key.toString();
-//            Text outputKey = new Text(output);
-//            context.write(key, new Text(val.toString()));
-////            throw new IOException("Key: " + key + " --- Val: " + val + "--- Vals: " + map.keySet().size());
-//        }
+
+        //calculate the number of books this word occurs
+        double bookOccurCount = map.size();
+
+//        Iterator<TextArrayWritable> iterator = values.iterator();
+        for (TextArrayWritable val : map.keySet()) {
+            Text title = (Text) val.get()[0];
+            Double wordCount = Double.parseDouble(val.get()[1].toString());
+            Double maxWordCount = Double.parseDouble(val.get()[2].toString());
+            //calculate TF-IDF value
+            Double tf = wordCount / maxWordCount;
+            Double idf = Math.log(totalBookCount / bookOccurCount) / Math.log(2);
+            DoubleWritable tfidf = new DoubleWritable(tf / idf);
+
+            String output = title.toString() + ":" + key.toString();
+            Text outputKey = new Text(output);
+            context.write(key, new Text(val.toString()));
+//            throw new IOException("Key: " + key + " --- Val: " + val + "--- Vals: " + map.keySet().size());
+        }
         }
     }
